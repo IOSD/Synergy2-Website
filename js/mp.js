3cebf201313d4eaa00f2d0a545d36637dd41067d
Dhaarna <dhaarna29@gmail.com>
2017-09-09 16:24:30 +0530

    fixed

---
+++ js/mp.js
@@ -0,0 +1,6 @@
+$("#aftermovie-pop").magnificPopup({
+         items: {
+             src: 'https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fiosddtu%2Fvideos%2F2370531866504599%2F&show_text=0'
+         },
+         type: 'iframe'   
+     });
